46e50b0492a9e9f18d826bc6a5f8b3529c8d6e18
aihara <>
2022-01-14 18:55:55 +0900

    add React components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout pageTitle="About me" pageHeading="About">
+      This is my about page
+    </Layout>
+  )
+}
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout pageTitle="Contact us" pageHeading="Contact">
+      Now is the time for all good people to come tothe aid of Sulli
+    </Layout>
+  )
+}
+
+export default ContactPage;
